575bb75cd72c5ed25e3f47f312caf9553ed2c102
Your Name <you@example.com>
2024-03-25 17:57:53 +0100

    deploy

--- client/src/socket.tsx
+++ client/src/socket.tsx
@@ -1,5 +1,8 @@
 import io from "socket.io-client"
-const url = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001'
+const url = process.env.REACT_APP_SERVER_URL
+if(!url){
+    throw new Error("REACT_APP_SERVER_URL is not defined")
+}
 const socket = io(url)
 
 export default socket
